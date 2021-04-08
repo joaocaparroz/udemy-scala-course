ef4580f9a1432a2ad08ab5663cda6c9d733f5c51
João Caparroz <joaocaparroz@gmail.com>
2021-04-08 16:53:14 -0300

    🤩 feature: Finished 2
    Finished 2

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,23 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42
+  println(x)
+
+  val aString: String = "Hello"
+  val anotherString: String = "Goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 421
+  val aLong: Long = 98029384892310L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 2.0
+
+  // Variables
+
+  var aVariable: Int = 4
+
+  aVariable = 5
+}
