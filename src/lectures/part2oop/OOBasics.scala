be78559905fdec54acead57b6fff492dc44dbf09
João Caparroz <joaocaparroz@gmail.com>
2021-04-16 18:58:03 -0300

    🤩 feature: Finished 10
    Finished 10

---
+++ src/lectures/part2oop/OOBasics.scala
@@ -0,0 +1,23 @@
+package lectures.part2oop
+
+object OOBasics extends App {
+
+  val person = new Person("João", 30)
+  println(person.age)
+  println(person.x)
+  person.greet("Daniel")
+  person.greet()
+}
+
+class Person (name: String, val age: Int = 0) {
+  val x = 2
+
+  println(1 + 3)
+
+  def greet(name: String): Unit = println(s"${this.name} says: Hi, $name!")
+
+  def greet(): Unit = println(s"Hi, I am ${this.name}!")
+
+  def this(name: String) = this(name, 0)
+  def this() = this("John Doe")
+}
