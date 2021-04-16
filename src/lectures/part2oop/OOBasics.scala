da1a11d2af7b10758bacc7bedf0b7107893cec1f
João Caparroz <joaocaparroz@gmail.com>
2021-04-16 19:26:21 -0300

    🤩 feature: Finished 11
    Finished 11

--- src/lectures/part2oop/OOBasics.scala
+++ src/lectures/part2oop/OOBasics.scala
@@ -1,5 +1,6 @@
 package lectures.part2oop
 
+
 object OOBasics extends App {
 
   val person = new Person("João", 30)
@@ -7,9 +8,16 @@ object OOBasics extends App {
   println(person.x)
   person.greet("Daniel")
   person.greet()
+
+  val author = new Writer("João", "Caparroz", 1990)
+  val impostor = new Writer("João", "Caparroz", 1990)
+  val novel = new Novel("Olar", 2002, author)
+
+  println(novel.authorAge)
+  println(novel.isWrittenBy(impostor))
 }
 
-class Person (name: String, val age: Int = 0) {
+class Person(name: String, val age: Int = 0) {
   val x = 2
 
   println(1 + 3)
@@ -19,5 +27,27 @@ class Person (name: String, val age: Int = 0) {
   def greet(): Unit = println(s"Hi, I am ${this.name}!")
 
   def this(name: String) = this(name, 0)
+
   def this() = this("John Doe")
+}
+
+class Novel(name: String, year_of_release: Int, val author: Writer) {
+
+  def authorAge: Int = this.year_of_release - this.author.year
+
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+
+  def copy(newYearOfRelease: Int): Novel = new Novel(this.name, newYearOfRelease, this.author)
+}
+
+class Writer(val first_name: String, val surname: String, val year: Int) {
+  def fullName(): String = s"$first_name $surname"
+}
+
+class Counter(val count: Int = 0) {
+  def inc = new Counter(count + 1)
+  def dec = new Counter(count - 1)
+
+  def inc(n: Int) = new Counter(count + n)
+  def dec(n: Int) = new Counter(count - n)
 }
