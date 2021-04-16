407c96b95147fe14693dbb5723666d2ca87a0e8a
João Caparroz <joaocaparroz@gmail.com>
2021-04-16 16:17:22 -0300

    🤩 feature: Finished 7
    Finished 7

---
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,22 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+
+  def callByValue(x: Long): Unit = {
+    println("by value: " + x)
+    println("by value: " + x)
+  }
+
+  def callByName(x: => Long): Unit = {
+    println("by name: " + x)
+    println("by name: " + x)
+  }
+
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+  printFirst(34, infinite())
+}
